97a1dc8721bcd4f786cd807af19e1daa491fdb00
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-26 14:19:21 -0800

    - new pfp ;) - fix command handler - fix picasso

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const { prefix } = require('./config.json');
 const { name } = require('./package.json');
 const keepAlive = require('./server');
 
-module.exports.client = client
+module.exports.client = client;
 
 // Command Handler
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
@@ -18,21 +19,32 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('actual bot is online');
+	client.user.setActivity('h', { type: 'CUSTOM_STATUS' }); // Status
 });
 
 // Commands
 client.on('message', (message) => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+	if (!message.content.startsWith(prefix)) return;
 
-	if (command === 'ping') {
-		client.commands.get('ping').execute(message, args);
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command =
+		client.commands.get(commandName) ||
+		client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('There was an error trying to execute that command!');
 	}
 });
-// coding in vr
+
 // Picasso
-require('./picasso.');
+require('./picasso');
 
 keepAlive();
 // Always Last
