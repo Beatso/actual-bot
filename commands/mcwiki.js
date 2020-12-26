0e5ef31a4009d1893740ad872f7b03cca72426d0
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-26 13:09:13 -0800

    cmds

---
+++ commands/mcwiki.js
@@ -0,0 +1,26 @@
+const { prefix, color } = require('../config.json');
+
+module.exports = {
+	name: 'mcwiki',
+	cooldown: 5,
+	execute(message, args) {
+		if (args.length != 0)
+			var embed = {
+				title: 'Search Minecraft Wiki',
+				description: `[Search Minecraft Wiki for \`${args.join(
+					' '
+				)}\`](https://minecraft.gamepedia.com/index.php?search=${args.join('+')})`,
+				color: color
+			};
+		else
+			var embed = {
+				title: 'Minecraft Wiki',
+				description: `[Link to Minecraft Wiki](https://minecraft.gamepedia.com/)\nTip: search for something in particular with \`${prefix}wiki [query]\``,
+				color: color
+			};
+		return message.channel.send({ embed: embed }).catch((error) => {
+			console.error(`Could not give wiki link to ${message.author.tag}.\n`, error);
+			message.reply('There was an error searching for that.');
+		});
+	}
+};
---
+++ commands/userinfo.js
@@ -0,0 +1,53 @@
+const { color } = require('../config.json');
+const { client } = require('../index.js');
+
+module.exports = {
+	name: 'userinfo',
+	aliases: [ 'about', 'user', 'profile' ],
+	usage: '<user>',
+	description: 'Gets info about a particular user.',
+	availableTo: '@everyone',
+	parameters:
+		'**User**: *optional* the id or @mention of the user you want info about. Defaults to the author if unspecified.',
+	execute(message, args) {
+		if (!args[0]) id = message.author.id;
+		else if (args[0].startsWith('<@') && args[0].endsWith('>')) id = args[0].substring(3, 21);
+		else if (args[0].startsWith('<@!') && args[0].endsWith('>')) id = args[0].substring(4, 21);
+		else if (args[0].length == 18) id = args[0];
+		else id = message.author.id;
+
+		if (!client.users.cache.get(id)) id = message.author.id;
+
+		const user = client.users.cache.get(id);
+		const member = message.guild.members.cache.get(id);
+
+		const responseEmbed = {
+			title: `User Info for ${user.tag}`,
+			fields: [
+				{
+					name: 'Joined Discord',
+					value: user.createdAt.toUTCString()
+				},
+				{
+					name: 'Joined Server',
+					value: member.joinedAt.toUTCString()
+				},
+				{
+					name: 'Avatar',
+					value: `[Link](${user.avatarURL({ format: 'png', dynamic: 'true' })})`
+				},
+				{
+					name: 'ID',
+					value: user.id
+				},
+				{
+					name: 'Nickname',
+					value: member.nickname == null ? 'None' : member.nickname
+				}
+			],
+			color: color
+		};
+
+		message.channel.send({ embed: responseEmbed });
+	}
+};
