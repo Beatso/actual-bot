b364385d5c56b494dab912d095a373a4857284c9
Chromus_ <Chromus-dev@users.noreply.github.com>
2021-02-01 20:19:25 -0800

    help command :)

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,49 @@
 const fs = require('fs');
+const { prefix } = require('../config.json');
 
 const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
 
 module.exports = {
 	name: 'help',
-	description: 'Lists Commands',
+	description: 'This command',
 	aliaes: [ 'commands', 'cmd', 'cmds', 'command' ],
 	execute(client, message, args, Discord) {
+		var cmdList = [];
+		var usage;
+
 		for (const file of commandFiles) {
 			const command = require(`../commands/${file}`);
-			if (command.name) {
-				// client.commands.set(command.name, command);
-			} else {
-				continue;
-			}
-		}
-
-		var cmdList = [ '1' ];
-
-		for (const file of commandFiles) {
-			const command = require(`./commands/${file}`);
 			if (command.name && command.description) {
 				cmdList.push(command.name);
 				cmdList.push(command.description);
+				if (command.usage) {
+					cmdList.push(prefix + command.usage);
+					usage = true;
+				} else {
+					usage = false;
+				}
 			} else {
 				continue;
 			}
 		}
-
-		console.log(commandFiles);
-		// console.log(cmdList);
 
 		const responseEmbed = new Discord.MessageEmbed()
 			.setColor('#7289da')
 			.setTitle('Commands for actual bot')
-			.addFields({ name: cmdList[0], value: cmdList[1] });
+			.setFooter('<required> [optional]');
+
+		if (usage) {
+			for (var i = 0; i < cmdList.length; i = i + 3) {
+				responseEmbed.addFields(
+					{ name: cmdList[i], value: cmdList[i + 1] },
+					{ name: cmdList[i + 2], value: '\u200B' }
+				);
+			}
+		} else {
+			for (var i = 0; i < cmdList.length; i = i + 2) {
+				responseEmbed.addField({ name: cmdList[i], value: cmdList[i + 1] });
+			}
+		}
 
 		message.channel.send(responseEmbed);
 	}
--- commands/nick.js
+++ commands/nick.js
@@ -1,12 +1,13 @@
 module.exports = {
 	name: 'nick',
 	description: "Changes the user's nickname",
+	usage: 'nick <user> [duration]',
 	execute(client, message, args, Discord) {
 		const target = message.mentions.users.first();
 		const member = message.guild.members.cache.get(target.id);
 		args.shift();
 		const nickname = args.join(' ');
 		member.setNickname(nickname);
-		message.channel.send('<@' + member + '>, <@' + message.author.id + '> nickname has been set to ' + nickname);
+		message.channel.send('<@' + member + '>, <@' + message.author.id + ">'s nickname has been set to " + nickname);
 	}
 };
