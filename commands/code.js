d8968bc22b86eb4086d4911e60ccd96792d1223a
Chromus_ <Chromus-dev@users.noreply.github.com>
2021-02-08 07:44:05 -0800

    fix image, add code command,  and rename play-music

---
+++ commands/code.js
@@ -0,0 +1,35 @@
+const sourcebin = require('sourcebin_js');
+
+module.exports = {
+	name: 'code',
+	description: 'Uploads code to sourcebin',
+	aliases: [ 'bin', 'sourcebin', 'srcbin', 'src' ],
+	usage: `code <code>`,
+	async execute(client, message, args, cmd) {
+		if (!args.join(' ')) return message.reply('You must specify code to upload.');
+
+		const originalCode = args.join(' ');
+		code = originalCode.substring(5, originalCode.length - 3);
+		codeLang = originalCode.substring(3, 5);
+
+		await sourcebin
+			.create(
+				[
+					{
+						content: code,
+						languageId: codeLang
+					}
+				],
+				{
+					title: `Code by ${message.author.tag}`,
+					description: `Automagically uploaded by actual bot.`
+				}
+			)
+			.then((src) => {
+				message.channel.send(src.url);
+			})
+			.catch((e) => {
+				message.reply(`There was an error uploading your code.`);
+			});
+	}
+};
