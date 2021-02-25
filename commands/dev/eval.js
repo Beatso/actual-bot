15de8cf95cdb427a12b236c57acdab6edba64283
Chromus_ <Chromus-dev@users.noreply.github.com>
2021-02-25 13:22:37 -0800

    even better fix

--- commands/dev/eval.js
+++ commands/dev/eval.js
@@ -8,12 +8,16 @@ module.exports = {
 		if (!message.author.id == '511758610720751626') return message.reply('This command is only for the developer.');
 		if (!args[0]) return message.reply('You must specify code to eval.');
 		var result = args.join(' ');
-		if (args.join(' ') == 'process.exit()' || args.join('').includes('client.token')) {
+		if (args.join(' ') == 'process.exit()') {
 			return message.reply('Access Denied.');
 		}
 		try {
 			evaled = await eval(`(async () => {${result}})()`);
-			message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
+			if (!evaled.includes(client.token)) {
+				message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
+			} else {
+				message.reply('The bot token is not available to the public.');
+			}
 		} catch (error) {
 			message.channel.send(`\`\`\`\n${error}\`\`\``);
 		}
